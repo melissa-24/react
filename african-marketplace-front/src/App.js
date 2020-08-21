37422a4d613cd7251eb82f3b547a6392dd327a30
Samantha Goodman <slgoodman89@gmail.com>
2020-08-21 00:30:58 -0400

    started yup schema

--- african-marketplace-front/src/App.js
+++ african-marketplace-front/src/App.js
@@ -6,7 +6,6 @@ function App() {
 	return (
 		<div className='App'>
 			<h1 style={{ margin: '0 auto' }}>African Marketplace</h1>
-			<ItemEntry />
 		</div>
 	);
 }
--- african-marketplace-front/src/components/ItemEntry.js
+++ african-marketplace-front/src/components/ItemEntry.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import * as yup from 'yup';
 
 //blank object for item data
 const blankState = {
@@ -7,28 +8,41 @@ const blankState = {
     description: '',
     category: '',
     location: ''
-}
+};
 
 export default function ItemEntry () {
 //form state
     const [formState, setFormState] = useState({...blankState});
 //button disabled state
-    const [btnDisabled, setBtndisabled] = useState(true);
+    const [btnDisabled, setBtnDisabled] = useState(false);
     //const [productState, setProductState] = useState({});
 
 //change handler
     const handleChanges = (event) => {
         event.persist();
         setFormState({...formState, [event.target.name] : event.target.value});
-        console.log('formstate val', formState);
+        console.log('formState val', formState);
     };
+
+//submit
+    const submitForm = (event) => {
+        event.preventDefault();
+        //test to see what data will be sent 🔽
+        console.log('data captured!', formState);
+    };
+
+//yup form schema
+const formSchema = yup.object().shape({
+    name: yup.string().required('Please enter your name!'),
+    
+});
 
     return (
         <section>
             <h2>Add Product</h2>
             <p>Please enter the following information.</p>
 {/* FORM */}
-            <form>
+            <form onSubmit= { submitForm }>
 {/* NAME INPUT*/}
                 <label htmlFor='name'>Product Name: </label>
                 <input id='name' name='name' type='text' placeholder='Rosecoco Beans' value={ formState.name } onChange={ handleChanges }/> 
@@ -44,14 +58,14 @@ export default function ItemEntry () {
                 <br />
 {/* PRICE INPUT*/}
                 <label htmlFor='price'>Price (USD): </label>
-                    <input id='price' name='price' type='number' placeholder='10.00' value={ formState.price } min='0.01' onChange={ handleChanges } />
+                    <input id='price' name='price' type='number' placeholder='10.00' step='0.05' value={ formState.price } min='0.01' onChange={ handleChanges } />
                 <br />
 {/* DESCRIPTION TEXTAREA*/}
                 <label htmlFor='description'>Description: </label>
                 <textarea id='description' name='description' value={ formState.description } placeholder='Please describe the product in 1-2 sentences.' onChange={ handleChanges }/>   
                 <br />
 {/* SUBMIT BUTTON*/}
-                <button disabled={ btnDisabled }>Add This Item</button>
+                <button disabled={ btnDisabled } type='submit'>Add This Item</button>
             </form>
         </section>
     )
