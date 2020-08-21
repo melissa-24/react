4041b3c833f69731ff43b8b3380719bb0ce93c70
Samantha Goodman <slgoodman89@gmail.com>
2020-08-20 22:00:53 -0400

    add necessary properties to form elements and create change handler

--- african-marketplace-front/src/components/ItemEntry.js
+++ african-marketplace-front/src/components/ItemEntry.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
+//blank object for item data
 const blankState = {
     name: '',
     price: '',
@@ -9,34 +10,45 @@ const blankState = {
 }
 
 export default function ItemEntry () {
-    const [productState, setProductState] = useState({...blankState})
-    console.log(productState)
+//form state
+    const [formState, setFormState] = useState({...blankState});
+    //const [productState, setProductState] = useState({});
+
+//change handler
+    const handleChanges = (event) => {
+        event.persist();
+        setFormState({...formState, [event.target.name] : event.target.value});
+        console.log('formstate val', formState);
+    };
+
     return (
         <section>
             <h2>Add Product</h2>
             <p>Please enter the following information.</p>
+{/* FORM */}
             <form>
-                <label>Product Name:
-                    <input type='text' />
-                </label>
+{/* NAME INPUT*/}
+                <label htmlFor='name'>Product Name: </label>
+                <input id='name' name='name' type='text' placeholder='Rosecoco Beans' value={ formState.name } onChange={ handleChanges }/> 
                 <br />
-                <label>Category:
-                    <select>
-                        <option>-- select a category --</option>
-                        <option>Animal Product</option>
-                        <option>Cereals</option>
-                        <option>Beans</option>
-                    </select>
-                </label>
+{/* CATEGORY INPUT*/}
+                <label>Category: </label>
+                <select id='category' name='category' value={ formState.category } onChange={ handleChanges } >
+                    <option value=''>-- choose a category --</option>
+                    <option value='animal product'>Animal Product</option>
+                    <option value='cereal'>Cereal</option>
+                    <option value='bean'>Bean</option>
+                </select>
                 <br />
-                <label>Price:
-                    <input type='text' /> USD
-                </label>
+{/* PRICE INPUT*/}
+                <label>Price (USD): </label>
+                    <input id='price' name='price' type='number' placeholder='10.00' value={ formState.price } min='0.01' onChange={ handleChanges } />
                 <br />
-                <label>Description:
-                    <textarea />
-                </label>
+{/* DESCRIPTION TEXTAREA*/}
+                <label>Description: </label>
+                <textarea id='description' name='description' value={ formState.description } placeholder='Please describe the product in 1-2 sentences.'/>   
                 <br />
+{/* SUBMIT BUTTON*/}
                 <button>Add This Item</button>
             </form>
         </section>
