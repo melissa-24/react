c5bbc2f8b8a3f0db6bb1cf21c5ec20550e8718ce
Samantha Goodman <slgoodman89@gmail.com>
2020-08-21 22:41:58 -0400

    re-add handleChanges event and value prop to each Field element

--- african-marketplace-front/src/components/ItemEntry.js
+++ african-marketplace-front/src/components/ItemEntry.js
@@ -15,21 +15,27 @@ export default function ItemEntry () {
 //form state
     const [formState, setFormState] = useState({...blankState});
 //button disabled state
-    const [btnDisabled, setBtnDisabled] = useState(false);
+    //const [btnDisabled, setBtnDisabled] = useState(false);
     //const [productState, setProductState] = useState({});
 
 //change handler
     const handleChanges = (event) => {
         event.persist();
         setFormState({...formState, [event.target.name] : event.target.value});
-        console.log('formState val', formState);
+   
     };
+
+    //logs CURRENT VALUE OF formState when changed
+    useEffect(() => {
+        console.log('formState val', formState);
+    }, [formState]);
 
 //submit
     const submitForm = (event) => {
         event.preventDefault();
         //test to see what data will be sent 🔽
         console.log('data captured!', formState);
+        
     };
 
 //yup form schema
@@ -38,85 +44,61 @@ const formSchema = yup.object().shape({
     price: yup.number().required().label('Price').min(0.99, 'Price must be at least 0.99 USD'),
     description: yup.string().required().label('Description').min(2, 'must be at least 2 characters'),
     category: yup.string().oneOf(['animal product', 'cereal', 'bean']).required(),
-    location: yup.string().min(3).max(3)
+    location: yup.string().min(3).max(3).required()
     
 });
 
     return (
         <section>
-            <div>
-                <h1>Formik Form!</h1>
-                <Formik 
-                initialValues={{...blankState}}
-                validationSchema={ formSchema }
-                onSubmit = { values => console.log('formik onSubmit values', values)}>
-                    {({ errors, touched }) => (
-                        <Form>
-                            <label htmlFor='name'>Product Name: </label>
-                            <Field id='name' name='name' type='text' placeholder='Rosecoco Beans'/>
-                            {errors.name && touched.name ? (
-                                <p style={{color:'red',fontWeight: 'bold'}}>
-                                    {errors.name}
-                                </p>
-                            ) : null }
-                            <label htmlFor='price'>Price: </label>
-                            <Field id='price' name='price' type='number' step='0.05' placeholder='10.00'/>
-                            {errors.price && touched.price ? (
-                                <p>{ errors.price }</p>
-                            ) : null }
-                            <label htmlFor='category'>Category: </label>
-                            <Field as='select' id='category' name='category'>
-                                <option value=''>-- choose a category --</option>
-                                <option value='animal product'>Animal Product</option>
-                                <option value='bean'>Bean</option>
-                                <option value='cereal'>Cereal</option>
-                            </Field>
-                            {errors.category && touched.category ? (
-                                <p>{ errors.category }</p>
-                            ) : null}
-                            <label htmlFor='description'>Description: </label>
-                            <Field id='description' name='description' as='textarea' placeholder='describe this item in 1-2 sentences'/>
-                            {errors.description && touched.description ? (
-                                <p>{ errors.description }</p>
-                            ) : null}
-                            <label htmlFor='location'>Location: </label>
-                            <Field  id='location' name='location' type='text' placeholder='KEN'/>
-                            {errors.location && touched.location ? (
-                                <p>{ errors.location }</p>
-                            ) : null}
-                            <button type='submit'>Add Product</button>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
             <h2>Add Product</h2>
             <p>Please enter the following information.</p>
-{/* FORM */}
-            <form >
-{/* NAME INPUT*/}
-                <label htmlFor='name'>Product Name: </label>
-                <input id='name' name='name' type='text' placeholder='Rosecoco Beans' value={ formState.name } /> 
-                <br />
-{/* CATEGORY INPUT*/}
-                <label htmlFor='category'>Category: </label>
-                <select id='category' name='category' value={ formState.category }  >
-                    <option value=''>-- choose a category --</option>
-                    <option value='animal product'>Animal Product</option>
-                    <option value='cereal'>Cereal</option>
-                    <option value='bean'>Bean</option>
-                </select>
-                <br />
-{/* PRICE INPUT*/}
-                <label htmlFor='price'>Price (USD): </label>
-                    <input id='price' name='price' type='number' placeholder='10.00' step='0.05' value={ formState.price } min='0.01'  />
-                <br />
-{/* DESCRIPTION TEXTAREA*/}
-                <label htmlFor='description'>Description: </label>
-                <textarea id='description' name='description' value={ formState.description } placeholder='Please describe the product in 1-2 sentences.' />   
-                <br />
-{/* SUBMIT BUTTON*/}
-                <button disabled={ btnDisabled } type='submit'>Add This Item</button>
-            </form>
+            <Formik 
+            initialValues={{...blankState}}
+            validationSchema={ formSchema }
+            onSubmit = { submitForm }>
+                {({ errors, touched }) => (
+                    <Form>
+                        <label htmlFor='name'>Product Name: </label>
+                        <Field id='name' name='name' type='text' placeholder='Rosecoco Beans' value = { formState.name } onChange= { handleChanges }/>
+                        {errors.name && touched.name ? (
+                            <p style={{color:'red',fontWeight: 'bold'}}>
+                                {errors.name}
+                            </p>
+                        ) : null }
+                        <br />
+                        <label htmlFor='price'>Price: </label>
+                        <Field id='price' name='price' type='number' step='0.05' placeholder='10.00' value = { formState.price } onChange= { handleChanges }/>
+                        {errors.price && touched.price ? (
+                            <p>{ errors.price }</p>
+                        ) : null }
+                        <br />
+                        <label htmlFor='category'>Category: </label>
+                        <Field as='select' id='category' name='category' value = { formState.category } onChange= { handleChanges }>
+                            <option value=''>-- choose a category --</option>
+                            <option value='animal product'>Animal Product</option>
+                            <option value='bean'>Bean</option>
+                            <option value='cereal'>Cereal</option>
+                        </Field>
+                        {errors.category && touched.category ? (
+                            <p>{ errors.category }</p>
+                        ) : null}
+                        <br />
+                        <label htmlFor='description'>Description: </label>
+                        <Field id='description' name='description' as='textarea' placeholder='describe this item in 1-2 sentences' value = { formState.description } onChange= { handleChanges }/>
+                        {errors.description && touched.description ? (
+                            <p>{ errors.description }</p>
+                        ) : null}
+                        <br />
+                        <label htmlFor='location'>Location: </label>
+                        <Field  id='location' name='location' type='text' placeholder='KEN' value = { formState.location } onChange= { handleChanges }/>
+                        {errors.location && touched.location ? (
+                            <p>{ errors.location }</p>
+                        ) : null}
+                        <br />
+                        <button type='submit'>Add Product</button>
+                    </Form>
+                )}
+            </Formik>       
         </section>
     )
 }
