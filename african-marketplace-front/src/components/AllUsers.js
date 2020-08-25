1fd04692b6f0837dcfbfd91b4695ade9fc00080d
Samantha Goodman <slgoodman89@gmail.com>
2020-08-25 13:43:34 -0400

    add array map, usestate/effect and some style

--- african-marketplace-front/src/components/AllUsers.js
+++ african-marketplace-front/src/components/AllUsers.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './AllUsers.css';
 
 function AllUsers() {
-    const [ userState, setUserState ] = useState({});
+    const [ userState, setUserState ] = useState([]);
 
     useEffect(() => {
         axios.get('https://reqres.in/api/users?page=2api/users')
@@ -16,7 +17,24 @@ function AllUsers() {
 
     return (
         <section>
-            <div>All Users Component</div>
+            <h2>All Users Component</h2>
+            <div className='users-container'>
+                {userState.map(user => {
+                    return (
+                        <div key={user.id} className='user-card'>
+                            <div className='user-card-left'>
+                                <img src={user.avatar} alt={`${user.first_name}'s profile`} />
+                                <p>username12345</p>
+                                <p><a href='#'>Visit Store</a></p>
+                            </div>
+                            <div className='user-card-right'>
+                                <p>{user.first_name} {user.last_name}</p>
+                                <p>{user.email}</p>
+                                <p>Location: USA</p>
+                            </div>
+                        </div>)
+                })}
+            </div>
             
         </section>
     );
