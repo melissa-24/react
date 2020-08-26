e3ab9dd2c894ff5ad744f64e2d5e235744023a01
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-08-26 11:12:08 -0400

    merge conflict

---
+++ african-marketplace-front/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import ItemEntry from './ItemEntry';
+import UserItems from './UserItems';
+import Registration from './Registration';
+import SignIn from './SignIn';
+
+class Navigation extends Component {
+	render() {
+		return (
+			<div className='nav-header'>
+				<Router>
+					<p>African Marketplace</p>
+					<ul id='nav'>
+						<li>
+							<a href='/'>Login</a>
+						</li>
+						<li>
+							<a href='/registration'>Register</a>
+						</li>
+						<li>
+							<a href='/sell'>Add Item</a>
+						</li>
+						<li>
+							<a href='/home'>All Items</a>
+						</li>
+					</ul>
+					<Switch>
+						<Route exact path='/' components={SignIn} />
+						<Route exact path='/Registration' components={Registration} />
+						<Route exact path='/sell' components={ItemEntry} />
+						<Route exact path='/home' components={UserItems} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
+}
+export default Navigation;
