5d5681a71a82f8b51e87d39ef75ad8c71b08bf34
Samantha Goodman <slgoodman89@gmail.com>
2020-08-25 20:57:24 -0400

    add style and display dummy data

--- african-marketplace-front/src/components/AllUsers.js
+++ african-marketplace-front/src/components/AllUsers.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './AllUsers.css';
+//import './AllUsers.css';
 
 function AllUsers() {
     const [ userState, setUserState ] = useState([]);
@@ -23,19 +23,18 @@ function AllUsers() {
                     return (
                         <div key={user.id} className='user-card'>
                             <div className='user-card-left'>
-                                <img src={user.avatar} alt={`${user.first_name}'s profile`} />
-                                <p>username12345</p>
+                                <img src={`https://picsum.photos/id/${user.id + 1001}/150`} alt={`${user.first_name}'s profile`} />                               
                                 <p><a href='#'>Visit Store</a></p>
                             </div>
                             <div className='user-card-right'>
+                                <h3>username12345</h3>
                                 <p>{user.first_name} {user.last_name}</p>
-                                <p>{user.email}</p>
-                                <p>Location: USA</p>
+                                <p>📧 {user.email}</p>
+                                <p>🌎 USA</p>
                             </div>
                         </div>)
                 })}
-            </div>
-            
+            </div>          
         </section>
     );
 }
