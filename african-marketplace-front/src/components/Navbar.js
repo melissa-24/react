089c364f6d7161e68a25a4bd1e7ac6cb7a18728c
Sharon Mosher <sharon.a.mosher@gmail.com>
2020-08-25 22:12:53 -0400

    Nav and Nav Styling in CSS

--- african-marketplace-front/src/components/Navbar.js
+++ african-marketplace-front/src/components/Navbar.js
@@ -1,1 +1,20 @@
-//TODO
+import React, { Component } from "react";
+ 
+class Navigation extends Component {
+  render() {
+    return (
+      <div className='nav-header'>
+        <p>African Market</p>
+       <ul id="nav">
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li>
+          <li><a href="#">Add Item</a></li>
+          <li><a href="#">All Items</a></li>
+          <li><a href="#">Log Out</a></li>
+       </ul>
+    </div>
+    );
+}
+}
+export default Navigation;
+
